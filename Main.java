e3e22765064d2d500d406159b68061d4aadd51e1
swasthiprakash <swasthiprakash@gmail.com>
2026-03-30 16:14:21 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Student Name: SWASTHIKA P");
+        System.out.println("Register Number: 23MIC0116");
+        System.out.println("Jenkins Build Successful");
+    }
+}
